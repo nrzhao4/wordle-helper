5b3145ac204df9767c0abc6cb8e2cad56d93d1a8
Nina Zhao <nzhao@thumbtack.com>
2022-03-03 10:46:09 -0500

    logic and valid input checker changes

--- wordlehelper.py
+++ wordlehelper.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*- 
 
 # this list keeps track of previous states of the filtered word list
+from gettext import find
+
+
 WORDLIST_HIST = []
 
 # this function is called on startup
-def onStart():    
+def start():    
     wordList = loadWords()
     intro = "Welcome to Wordle Helper - here to help you solve your daily Wordle!"
     print(intro)
@@ -37,10 +40,9 @@ Type help then press ENTER at any time to see the list of available commands.'''
 
 # use input to filter word list
 def findWords(words):
-    while True:
         if len(words) == 0:
             print("Sorry, no more words match your criteria")
-            break
+            return
 
         userInput = input("Type a command to start filtering words or type help the press ENTER: ")
         userInput = userInput.lower().split(' ')
@@ -48,57 +50,71 @@ def findWords(words):
 
         # special commands
         if command == "quit":
-            break
-        if command == "help":
+            return
+        elif command == "help":
             printInstructions()
-            continue
-        if command == "restart":
+        elif command == "restart":
             loadWords()
             print("Word list reset")
-            continue
-        if command == "back":
+        elif command == "back":
             words = WORDLIST_HIST.pop(len(WORDLIST_HIST)-1)
             printList(words)
-            continue
+        
         # check if input is valid
         if isValid(userInput) != True:
             print("Invalid user input. Try again")
-            continue
-
-        letters = userInput[1]
-        secondaryArgs = []
-        # optional secondary argument given with contains command
-        if len(userInput) > 2:
-            for x in range(2,4):
-                secondaryArgs.append(userInput[x])
-
-        # filter words
-        WORDLIST_HIST.append(words)
-        words = filterWords(words, command, letters, secondaryArgs)
-        printList(words)
+        else:
+            letters = userInput[1]
+            secondaryArgs = []
+            # optional secondary argument given with contains command
+            if len(userInput) > 2:
+                for x in range(2,4):
+                    secondaryArgs.append(userInput[x])
+            # filter words
+            WORDLIST_HIST.append(words)
+            words = filterWords(words, command, letters, secondaryArgs)
+            printList(words)
+        findWords(words)
                     
 
-#this function checks if the user input is valid
+# this function checks if the user input is valid
 def isValid(userInput):
+    command = userInput[0]
+    letters = userInput[1]
+
+    # valid commands
     nonNumbered = {"no", "contains"}
     numbered = {"first", "second", "third", "fourth", "fifth"}
-    # check command is valid
-    if userInput[0] not in nonNumbered and userInput[0] not in numbered:
-        print("Enter a valid command")
-        return False
+    
     # check that input has correct number of arguments
-    if userInput[0] != "contains" and len(userInput) != 2:
+    if len(userInput) < 2:
         print("Your input does not have the correct number of arguments")
         return False
-    # for commands other than no and contains, only one letter can be included
-    if userInput[0] in numbered and len(userInput[1]) > 1:
+        # for commands other than no and contains, only one letter can be included
+    if command != "contains" and len(userInput) > 2:
         print("The commands <first/second/third/fourth/fifth> can only include one letter")
         return False
-    # the contains command can be followed by an additional 'not <first/second/third/fourth/fifth>'
-    if userInput[0] == "contains" and len(userInput) != 2:
-        if len(userInput) != 4:
+
+    secondary = []
+    if len(userInput) > 2:
+        for x in range(2, len(userInput)):
+            secondary.append(userInput[x])
+
+    # check command is valid
+    if command not in nonNumbered and command not in numbered:
+        print("Enter a valid command")
+        return False
+    # check that input does not include numbers
+    for char in letters:
+        if char.isnumeric():
+            print("Oops! Looks like you entered a numerical value instead of a letter")
             return False
-        if userInput[2] != "not" or userInput[3] not in numbered:
+    # the contains command can be followed by an additional 'not <first/second/third/fourth/fifth>'
+    if command == "contains" and len(secondary) > 0:
+        if len(secondary) != 2:
+            print("Your input does not have the correct number of arguments")
+            return False
+        if secondary[0] != "not" or secondary[1] not in numbered:
             return False
     return True
 
@@ -140,14 +156,14 @@ def printList(words):
                     print(words[x-i], end = " ")
                 print("\n")
 
-#end program       
-def onEnd():
+# end program       
+def end():
     print("\nThanks for using World Helper!")
     quit()
 
 def main():
-    onStart()
-    onEnd()
+    start()
+    end()
 
 
 if __name__ == "__main__": 
